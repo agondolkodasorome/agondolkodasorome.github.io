e8ede1c81cbb91cd010a349276957aa36f8e5292
Almási Péter <almasipeti715@gmail.com>
2020-07-04 17:33:05 +0200

    after merge conflict (hopefully...)

--- _site/assets/js/scripts.js
+++ _site/assets/js/scripts.js
@@ -2,8 +2,32 @@ var body = document.querySelector('body')
 var menuTrigger = document.querySelector('#toggle-main-menu-mobile');
 var menuContainer = document.querySelector('#main-menu-mobile');
 
+
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+  console.log("MyFunction");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(e) {
+  
+  var myDropdown = document.getElementById("myDropdown");
+  var myBtn = document.getElementById("myBtn");
+  
+  if (e.target != myBtn) {
+    if (myDropdown.classList.contains('show')) {
+	  myDropdown.classList.remove('show');
+	  console.log('removed');
+    }
+  }
+  
+}
+
+
 menuTrigger.onclick = function() {
     menuContainer.classList.toggle('open');
     menuTrigger.classList.toggle('is-active')
     body.classList.toggle('lock-scroll')
 }
+
+
